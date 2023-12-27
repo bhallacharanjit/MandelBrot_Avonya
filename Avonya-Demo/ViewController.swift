59acb8fcd5c0ab5959de850cc363e43fa3ef9699
Charanjit Singh Bhalla <bhallacharanjit@gmail.com>
2023-12-27 23:49:44 +0530

    Initial Commit

---
+++ Avonya-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avonya-Demo
+//
+//  Created by charanjit singh on 27/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
